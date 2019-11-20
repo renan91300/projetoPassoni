d75c0fa7713ed93bc38d404d66fe4a3d1549328e
renan91300 <renanpoggiangomes@gmail.com>
2019-11-20 20:50:58 -0300

    Consertando a seção da página sobre nós

--- app/site/views/sobreNos/index.php
+++ app/site/views/sobreNos/index.php
@@ -18,39 +18,39 @@ if (!defined('URL')){
     </div>
     
 </div>
-<hr><br>
-<section class="aboutussec">
-    <div class="idPresent anima_left">
+<hr>
+<div class="aboutussec row">
+    <div class="idPresent anima_left col-md-6">
         <p>Estamos presentes no seu
             <br>cotidiano também!</p><br>
         <span>Bolos de potão congeláveis</span>
     </div>
-    <div>
-        <img src="<?=URL;?>/assets/img/bolos/cafe.jpeg" id="imgCafe">
+    <div class="imgSec col-md-6 d-none d-md-block">
+        <!--<img src="<?=URL;?>/assets/img/bolos/cafe.jpeg" id="imgCafe">-->
     </div>
+</div>
 
-</section>
 <hr>
-<section class="sectionaboutus">
-    <br>
+
+<div class="sectionaboutus">
     <div class="row">
-        <div class="col">
+        <div class="anima_left col">
             <p class="cardtitle">Missão</p>
             <p class="cardsubtitle">Nossa missão é são bla bla bla</p>
             <hr>
         </div>
 
-        <div class="col">
+        <div class="anima_bottom col">
             <p class="cardtitle">Valores</p>
             <p class="cardsubtitle">Nossos valores são são bla bla bla</p>
             <hr>
         </div>
 
 
-        <div class="col">
+        <div class="anima_right col">
             <p class="cardtitle">Visão</p>
             <p class="cardsubtitle">Nossa visão são bla bla bla</p>
             <hr>
         </div>
     </div>
-</section>
+</div>
