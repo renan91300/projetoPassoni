9e1d5897ae143ecbff9db66cdbdef80e4b18192c
Renan <renanpoggiangomes@gmail.com>
2019-12-01 20:18:03 -0300

    antigo notebook

--- app/site/controllers/Bolopote.php
+++ app/site/controllers/Bolopote.php
@@ -12,8 +12,18 @@ class BoloPote{
 	
     public function index(){
     	$listar_bolosdepote = new \Site\models\Produto();
-	    $this->dados['bolos'] = $listar_bolosdepote->listar();
+        $this->dados['bolos'] = $listar_bolosdepote->listar();
+        //var_dump($this->dados['bolos']);
+        //exit;
 
+        echo json_encode(
+            array(
+                "idBoloDePote" => $this->dados['bolos'][0]['idBoloDePote'], 
+                "sabor" => $this->dados['bolos'][0]['sabor'], 
+                )
+        );
+        
+        
         $carregarView = new \Config\ConfigView("boloPote/index", $this->dados);
         $carregarView->renderizar();
     }
